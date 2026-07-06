a59baa839669b5a33a90785426617ad3dfe47640
gjycc <gjycosmeticconsulting>
2026-07-06 12:36:50 -0300

    arruine todo

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { Footer } from './components/Footer/Footer';
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
 import { CartView } from './components/cart/CartView';
+import { ProductSuccess } from './components/adminComponent/ProductSuccess';
+import { ProductFormContainer } from './components/adminComponent/ProductFormContainer';
 
 
 function App() {
@@ -18,8 +20,9 @@ function App() {
           <Route path='/' element={<ItemListContainer />} />
           <Route path='/product/:id' element={<ItemDetailContainer />} />
           <Route path='/Carrito' element={<CartView />} />
-          <Route path='/admin' element={<h1>Admin</h1>} />
-          <Route path='/success/:id' element={<h1>Producto cargado</h1>} />
+          <Route path='/admin' element={<ProductFormContainer />} />
+          <Route path='/success/:id' element={<ProductSuccess />} />
+          <Route path= 'products/new' element={<ProductFormContainer />} />
         </Routes> 
       </main>
       <Footer />
